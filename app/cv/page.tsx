c6f7519b00fa707cdb9d6f8011dd23a49bfbdb99
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-04-17 21:20:14 -0400

    updated cv page

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -8,17 +8,17 @@ export default function CV() {
 
             <section>
                 <h2 className="font-medium">Education</h2>
-                <p>University Name — Degree — Years</p>
+                <p>University of Toronto — Master&apos;s in Engineering — 2023-2025</p>
             </section>
 
             <section>
                 <h2 className="font-medium">Experience</h2>
-                <p>Job Title — Company — Years</p>
+                <p>SWE - LeddarTech - 2023-2024</p>
             </section>
 
             <section>
                 <h2 className="font-medium">Skills</h2>
-                <p>React, Next.js, TypeScript, Tailwind CSS</p>
+                <p>C++, Go, Java, Bash, React, NextJS, TypeScript</p>
             </section>
 
             <a href="./CV_base.pdf" target="_blank" className="underline">
