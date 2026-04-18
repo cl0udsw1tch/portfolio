3f127639d87e68c3ff622de830cc4842031e3f3c
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-04-17 21:15:19 -0400

    fixed routes in papers page

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link"
 
-const BLOG_URL = "http://example.com"
+const BLOG_URL = "http://tech0.vercel.app"
 
-const Paper = (props: { name: string, date: string }) => {
+const Paper = (props: { name: string, date: string, blog_route: string }) => {
     return (
         <li >
-            <Link href={new URL(BLOG_URL + "/" + props.name).toString()} className="font-medium" style={{ margin: '10px' }}>{props.name} ({props.date})</Link>
+            <Link href={new URL(BLOG_URL + "/" + props.blog_route).toString()} className="font-medium" style={{ margin: '10px' }}>{props.name} ({props.date})</Link>
         </li>
     )
 
 }
 
 const papers = [
-    { name: "Backpropogation with Tensors", date: "March 1 2026" },
-    { name: "A Steady State Incompressible Fluid Flow Solver using the SIMPLE method", date: "February 1 2025" }
+    { name: "Backpropogation with Tensors", date: "March 1 2026", blog_route: "tensor-backprop" },
+    { name: "A Steady State Incompressible Fluid Flow Solver using the SIMPLE method", date: "February 1 2025", blog_route: 'fluid-flow-solver' }
 ]
 
 export default function Papers() {
@@ -21,7 +21,7 @@ export default function Papers() {
         <div className="space-y-6">
             <h1 className="text-2xl font-semibold">Papers</h1>
             <ul style={{ listStyleType: 'circle' }}>
-                {papers.map((k) => <Paper name={k.name} date={k.date} key={k.name} />)}
+                {papers.map((k) => <Paper name={k.name} date={k.date} blog_route={k.blog_route} key={k.name} />)}
 
             </ul>
         </div>
