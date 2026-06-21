70c0a627706d6746a759ebbfff1b2ecc4d0157ff
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-06-21 12:28:45 -0400

    fixed routes

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link"
 
-const BLOG_URL = "http://tech0.vercel.app"
+const BLOG_URL = "http://thecontraction.vercel.app"
 
 const Paper = (props: { name: string, date: string, blog_route: string }) => {
     return (
-        <li >
-            <Link href={new URL(BLOG_URL + "/" + props.blog_route).toString()} className="font-medium" style={{ margin: '10px' }}>{props.name} ({props.date})</Link>
+        <li style={{ display: 'flex', justifyContent: 'space-between' }} >
+            <Link href={new URL(BLOG_URL + "/" + props.blog_route).toString()} className="font-medium" style={{ margin: '1px' }}>{props.name} </Link>
+            <span>{props.date}</span>
         </li>
     )
 
 }
 
 const papers = [
-    { name: "Tensors", date: "April 7 2026", blog_route: "tensors" },
-    { name: "Backpropogation with Tensors", date: "March 1 2026", blog_route: "tensor-backprop" },
-    { name: "A Steady State Incompressible Fluid Flow Solver using the SIMPLE method", date: "February 1 2025", blog_route: 'fluid-flow-solver' }
+    { name: "Tensors-II", date: "April 7 2026", blog_route: "paper/tensors-II" },
+    { name: "Tensors-I", date: "April 7 2026", blog_route: "paper/tensors-I" },
+    { name: "Backpropogation with Tensors", date: "March 1 2026", blog_route: "paper/tensor-backprop" },
+    { name: "A Steady State Incompressible Fluid Flow Solver using the SIMPLE method", date: "February 1 2025", blog_route: 'paper/fluid-flow-solver' }
 ]
 
 export default function Papers() {
